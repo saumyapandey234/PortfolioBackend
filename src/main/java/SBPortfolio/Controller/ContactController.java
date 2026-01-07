a6bf141724ca80ff46133be89bb8b363602f94c1
Saumya <soumya1899pandit@gmail.com>
2026-01-07 15:37:51 +0530

    Add CORS support for frontend access

--- src/main/java/SBPortfolio/Controller/ContactController.java
+++ src/main/java/SBPortfolio/Controller/ContactController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class ContactController {
 
   @Autowired
